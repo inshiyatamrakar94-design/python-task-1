a7f91394ee94a3a90477a0cd41b54df3b0f6d33d
inshiyatamrakar94-design <inshiyatamrakar94@gmail.com>
2026-05-03 18:24:49 +0545

    file 2 feri

---
+++ second program file/fibonacci series.py
@@ -0,0 +1,62 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#fibonacci series
+a,b = 0,1
+while a<10:
+    print (a)
+    a,b= b,a+b
+
+    
+0
+1
+1
+2
+3
+5
+8
+>>> #fibonacci series  self
+>>> a,b=0,1
+>>> while a<20:
+...     print (a)
+...     a,b= b,a+b
+... 
+...     
+0
+1
+1
+2
+3
+5
+8
+13
+>>> 
+>>> #fibonacci series self 2
+>>> a,b=0,1
+>>> while a<100
+SyntaxError: expected ':'
+>>> while a<100
+SyntaxError: expected ':'
+>>> 
+>>> 
+>>> while a<100:
+...     print (a)
+...     a,b= b, a+b
+... 
+...     
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89  
+
+
+
+
+
